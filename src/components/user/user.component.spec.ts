897ea5143dab5608864d4b87b38babc03790df30
Kvezal <muckam49@mail.ru>
2020-12-04 11:15:09 +0800

    feat(components): user component created and fonts added to project (component-1)

---
+++ src/components/user/user.component.spec.ts
@@ -0,0 +1,54 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { UserComponent } from './user.component';
+
+
+describe(`UserComponent`, () => {
+  let component: UserComponent;
+  let fixture: ComponentFixture<UserComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ UserComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(UserComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it(`should create`, () => {
+    expect(component).toBeTruthy();
+  });
+
+  describe(`"email" input`, () => {
+    it(`shouldn't exist email container if email is empty`, () => {
+      const emailContainer = fixture.nativeElement.querySelector(`.user__email`);
+      expect(emailContainer).toBeNull();
+    });
+
+    it(`should set email in email container`, () => {
+      component.email = `test-email`;
+      fixture.detectChanges();
+      const emailContainer = fixture.nativeElement.querySelector(`.user__email`);
+      expect(emailContainer.textContent).toBe(`test-email`);
+    });
+  });
+
+  describe(`"avatar" input`, () => {
+    it('avatar image should be placeholder by default', () => {
+      const avatarContainer = fixture.nativeElement.querySelector(`.user__avatar-wrapper img`);
+      expect(avatarContainer.src).toMatch(`assets/images/avatar.svg`);
+    });
+
+    it(`should set avatar image`, () => {
+      component.avatar = `assets/images/test/1.jpg`;
+      fixture.detectChanges();
+      const avatarContainer = fixture.nativeElement.querySelector(`.user__avatar-wrapper img`);
+      expect(avatarContainer.src).toMatch(`assets/images/test/1.jpg`);
+    });
+  });
+});
