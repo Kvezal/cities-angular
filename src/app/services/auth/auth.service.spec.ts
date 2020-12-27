fbe8da7d00bcad2a0ec23e0056b0dd4e38b78a57
Kvezal <muckam49@mail.ru>
2020-12-27 14:34:51 +0800

    feat(services): auth service created (services-3)

---
+++ src/app/services/auth/auth.service.spec.ts
@@ -0,0 +1,114 @@
+import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+
+import {
+  ApiModule,
+  AuthApiService,
+  IAuthLoginParams,
+  IUserResponse
+} from '@api';
+
+import { AuthService } from './auth.service';
+
+
+const userInfo: IUserResponse = {
+  id: `023dda52-f07b-47ef-a44c-2301f8743149`,
+  name: `kvezal`,
+  email: `kvezal@gmail.com`,
+  image: `path/to/image.jpg`,
+};
+
+const authLoginParams: IAuthLoginParams = {
+  email: `test@gmail.com`,
+  password: `123456`,
+};
+
+describe(`AuthService`, () => {
+  let service: AuthService;
+  let authApiService: AuthApiService;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [ApiModule],
+      providers: [
+        {
+          provide: AuthApiService,
+          useValue: {
+            login: () => of(undefined),
+            logout: () => of(undefined),
+            loadAuthUserInfo: () => of(userInfo),
+          }
+        }
+      ],
+    });
+    service = TestBed.inject(AuthService);
+    authApiService = TestBed.inject(AuthApiService);
+  });
+
+  it(`should be created`, () => {
+    expect(service).toBeTruthy();
+  });
+
+  describe(`loadUserInfo`, () => {
+    it(`should call loadAuthUserInfo method of AuthApiService`, () => {
+      const loadAuthUserInfo = spyOn(authApiService, `loadAuthUserInfo`).and.callThrough();
+      service.loadUserInfo();
+      expect(loadAuthUserInfo).toHaveBeenCalledTimes(1);
+    });
+
+    it(`should emit user$ observable`, () => {
+      let user: IUserResponse;
+      service.user$.subscribe((userInfoResponse: IUserResponse) => {
+        user = userInfoResponse;
+      });
+      service.loadUserInfo();
+      expect(user).toEqual(userInfo);
+    });
+  });
+
+  describe(`login`, () => {
+    it(`should call login method of AuthApiService`, () => {
+      const login = spyOn(authApiService, `login`).and.callThrough();
+      service.login(authLoginParams);
+      expect(login).toHaveBeenCalledTimes(1);
+    });
+
+    it(`should call login method of AuthApiService with params`, () => {
+      const login = spyOn(authApiService, `login`).and.callThrough();
+      service.login(authLoginParams);
+      expect(login).toHaveBeenCalledWith(authLoginParams);
+    });
+
+    it(`should call loadAuthUserInfo method of AuthApiService`, () => {
+      const loadAuthUserInfo = spyOn(authApiService, `loadAuthUserInfo`).and.callThrough();
+      service.login(authLoginParams);
+      expect(loadAuthUserInfo).toHaveBeenCalledTimes(1);
+    });
+
+    it(`should emit user$ observable`, () => {
+      let user: IUserResponse;
+      service.user$.subscribe((userInfoResponse: IUserResponse) => {
+        user = userInfoResponse;
+      });
+      service.login(authLoginParams);
+      expect(user).toEqual(userInfo);
+    });
+  });
+
+  describe(`logout`, () => {
+    it(`should call logout method of AuthApiService`, () => {
+      const logout = spyOn(authApiService, `logout`).and.callThrough();
+      service.logout();
+      expect(logout).toHaveBeenCalledTimes(1);
+    });
+
+    it(`should emit null from user$ observable`, () => {
+      let user: IUserResponse;
+      service.user$.subscribe((userInfoResponse: IUserResponse) => {
+        user = userInfoResponse;
+      });
+      service.logout();
+      expect(user).toBeNull();
+    });
+  });
+});
---
+++ src/app/services/auth/auth.service.ts
@@ -0,0 +1,47 @@
+import { Injectable } from '@angular/core';
+import {
+  Observable,
+  ReplaySubject
+} from 'rxjs';
+import { switchMap } from 'rxjs/operators';
+
+import {
+  AuthApiService,
+  IAuthLoginParams,
+  IUserResponse
+} from '@api';
+
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthService {
+  private readonly _userReplaySubject: ReplaySubject<IUserResponse> = new ReplaySubject(1);
+  public readonly user$: Observable<IUserResponse> = this._userReplaySubject.asObservable();
+
+
+  constructor(private readonly _authApiService: AuthApiService) {
+    this.loadUserInfo();
+  }
+
+
+  public loadUserInfo(): void {
+    this._authApiService.loadAuthUserInfo()
+      .subscribe((user: IUserResponse) => this._userReplaySubject.next(user));
+  }
+
+
+  public login(params: IAuthLoginParams): void {
+    this._authApiService.login(params)
+      .pipe(
+        switchMap(() => this._authApiService.loadAuthUserInfo())
+      )
+      .subscribe((user: IUserResponse) => this._userReplaySubject.next(user));
+  }
+
+
+  public logout(): void {
+    this._authApiService.logout()
+      .subscribe(() => this._userReplaySubject.next(null));
+  }
+}
